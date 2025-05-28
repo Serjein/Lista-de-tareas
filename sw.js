53536f50e2941ef07ad08f2e7ca69f829a4b0b4b
Iskander C <serjein12@gmail.com>
2025-05-27 19:00:05 -0600

    cambio nombres

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('tareas-store').then(function(cache) {
       return cache.addAll([
-        'index.html',
+        'main.html',
         'style.css',
         'script.js',
         'manifest.json',
